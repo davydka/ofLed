a6a16b98d43b0960dba17510674f19ba66e708d0
davydka <davydka@gmail.com>
2018-07-27 13:40:47 -0400

    Visual glitches in osx preview. Debugging.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -16,7 +16,6 @@ void ofApp::setup() {
 	numPorts = 1;                               // total number of teensy ports?
 	brightness = 255;                             // LED brightness
 
-
 	drawModes = 0;                              // default mode
 
 	rectWidth = 1;
@@ -34,25 +33,11 @@ void ofApp::setup() {
 
 	// allocate our pixels, fbo, and texture
 	fbo.allocate(stripWidth, stripHeight*stripsPerPort*numPorts, GL_RGB);
-
-	setupMedia();
-}
-
-void ofApp::exit(){
-	/* turn all leds to black */
-	/*
-	fbo.begin();
-	ofClear(0,0,0);
-	fbo.end();
-	fbo.readToPixels(teensy.pixels1);
-	teensy.update();
-	*/
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	//ofSetWindowTitle("TeensyOctoExample - "+ofToString(ofGetFrameRate()));
-	ballpos+=dir*0.5f;
+	ballpos+=dir*1.0f;
 
 	updateFbo();                                // update our Fbo functions
 	teensy.update();                            // update our serial to teensy stuff
@@ -67,17 +52,7 @@ void ofApp::updateFbo(){
 	ofSetColor(255);
 
 	ofPushStyle();
-	switch (drawModes)
-	{
-		case 0:            
-			drawPong();
-			break;
-		case 1:
-			drawImages();
-			break;
-		default:
-			break;
-	}
+	drawPong();
 	ofPopStyle();
 
 	fbo.end();                                  // closes the fbo
@@ -88,11 +63,9 @@ void ofApp::updateFbo(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 	teensy.draw(0,0);
+	// fbo.draw(0, 0);
 
 	ofSetColor(255);
-	// ofDrawBitmapString("// Controls //", ofGetWidth()-250, 20);
-	// ofDrawBitmapString("Brightness (up/down) == " + ofToString(brightness), ofGetWidth()-250, 80);
-	// ofDrawBitmapString("Images # == " + ofToString(dirImg.size()), ofGetWidth()-250, 140);
 }
 
 void ofApp::drawPong(){
@@ -105,76 +78,11 @@ void ofApp::drawPong(){
 		dir = 1;
 	}
 	ofSetColor(255, 0, 255);
-	//ofSetColor(brightness);
 	ofDrawRectangle(ballpos,0,rectWidth,stripHeight*stripsPerPort*numPorts);
 }
 
 //--------------------------------------------------------------
-void ofApp::drawImages(){
-	if (dirImg.size() > 0) {
-		//img[currentImage].draw(0, 0, stripWidth, stripHeight*stripsPerPort*numPorts);
-		img[currentImage].draw(0, 0, stripWidth, rowHeight);
-	}
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){ 
-	switch (key){
-		//-----------------------------------------------
-		case OF_KEY_UP:
-			brightness += 2;
-			if (brightness > 255) brightness = 255;
-			teensy.setBrightness(brightness);
-			break;
-			
-		case OF_KEY_DOWN:
-			brightness -= 2;
-			if (brightness < 0) brightness = 0;
-			teensy.setBrightness(brightness);
-			break;
-
-		case 'i':
-			drawModes = 1;                          // image mode
-
-			img[currentImage].load(dirImg.getPath(currentImage));
-			break;
-		
-		case '=':
-			if (drawModes == 1) {
-				currentImage++;
-				if (currentImage > dirImg.size()-1) currentImage = 0;
-				img[currentImage].load(dirImg.getPath(currentImage));
-			}
-			break;
-			
-		case '-':
-			if (drawModes == 1) {
-				currentImage--;
-				if (currentImage < 0) currentImage = dirImg.size()-1;
-				img[currentImage].load(dirImg.getPath(currentImage));
-			}
-			break;
-			
-		case 't':
-			if (drawModes != 0) drawModes = 0;      // switch the draw mode
-			break;
-	}
-
-}
-
-//--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void ofApp::setupMedia(){
-	dirImg.listDir("images/");
-	dirImg.sort();
-	//allocate the vector to have as many ofImages as files
-	if( dirImg.size() ){
-		img.assign(dirImg.size(), ofImage());
-	}
-	currentImage = 0;
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,8 +9,8 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-        void exit();
-		void keyPressed(int key);
+		/*void exit();*/
+		/*void keyPressed(int key);*/
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
@@ -20,57 +20,57 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 
-        void updateFbo();
-        void setupMedia();
-        void enableVideo();
-        void disableVideo();
-        void drawPong();
+		void updateFbo();
+		/*void setupMedia();*/
+		void enableVideo();
+		void disableVideo();
+		void drawPong();
     
-    // LED - Teensy stuff
-    //-----------------------------
-    ofxTeensyOcto teensy;
-    int stripWidth;
-    int stripHeight;
-    int rowHeight;
-    int stripsPerPort;
-    int numPorts;
-    int brightness;
-    
-    // FBO stuff
-    //-----------------------------
-    ofFbo fbo;
-    int fboRotate;
-    bool fboFlip;
-    ofTexture tex;
-    ofPixels guiPixels;
-    
-    // Gui
-    //-----------------------------
-    void drawPanels();
-    ofTrueTypeFont font;
-    
-    // Graphic functions
-    //-----------------------------
-    void drawDemos();
-    void drawVideos();
-    void drawImages();
-    int drawModes;
-    int demoModes;
-    int rectWidth;
-    float ballpos;
-    int dir;
-
-    // Video player
-    //-----------------------------
-    ofDirectory dirVid;
-    vector<ofVideoPlayer> vid;
-    int currentVideo;
-    bool videoOn;
-    
-    // Image player
-    //-----------------------------
-    ofDirectory dirImg;
-    vector<ofImage> img;
-    int currentImage;
+		// LED - Teensy stuff
+		//-----------------------------
+		ofxTeensyOcto teensy;
+		int stripWidth;
+		int stripHeight;
+		int rowHeight;
+		int stripsPerPort;
+		int numPorts;
+		int brightness;
 		
+		// FBO stuff
+		//-----------------------------
+		ofFbo fbo;
+		int fboRotate;
+		bool fboFlip;
+		ofTexture tex;
+		ofPixels guiPixels;
+		
+		// Gui
+		//-----------------------------
+		void drawPanels();
+		ofTrueTypeFont font;
+		
+		// Graphic functions
+		//-----------------------------
+		void drawDemos();
+		void drawVideos();
+		void drawImages();
+		int drawModes;
+		int demoModes;
+		int rectWidth;
+		float ballpos;
+		int dir;
+
+		// Video player
+		//-----------------------------
+		ofDirectory dirVid;
+		vector<ofVideoPlayer> vid;
+		int currentVideo;
+		bool videoOn;
+		
+		// Image player
+		//-----------------------------
+		ofDirectory dirImg;
+		vector<ofImage> img;
+		int currentImage;
+			
 };
