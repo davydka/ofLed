e65867eefa5fd046797bf51de0c2e07021a76b73
davydka <davydka@gmail.com>
2018-08-23 18:27:18 -0400

    setting up pgm change actions on channel 6

--- pgmManager/index.js
+++ pgmManager/index.js
@@ -1,25 +1,70 @@
-const config = require('dotenv').config()
-var easymidi = require('easymidi');
+const config = require('dotenv').config();
+const easymidi = require('easymidi');
+const pm2 = require('pm2');
+const fs = require('fs');
 
 if (config.error) {
 	throw config.error;
 }
 
 const {
-	INDEX
+  INDEX
 } = process.env;
-console.log('pifull #:', INDEX);
+// console.log('pifull #:', INDEX);
+
+let currentPgm = fs.readFileSync('PGM').toString();
 
 
 easymidi.getInputs().forEach(function(inputName){
   var input = new easymidi.Input(inputName);
-  /*input.on('noteon', function (msg) {
-    console.log(inputName+": "+msg);
-  });
-  input.on('cc', function (msg) {
-    console.log(inputName+": "+msg);
-  });*/
   input.on('program', function (msg) {
-    console.log(inputName+": "+msg);
+
+    // 0 index midi channels, so channel 6 on device is channel 5 here
+    if(msg.channel === 5) {
+      handlePgm(msg.number);
+    }
+    else {
+      console.log(msg);
+    }
+
   });
 });
+
+function handlePgm(pgm) {
+  if(pgm === currentPgm) {
+    console.log('samesies');
+    return;
+  }
+  console.log(currentPgm);
+
+  let startProcess = pgm.toString().padStart(3, '0');
+  let stopProcess = "ofLed";
+  if(currentPgm.toString().indexOf("ofLed") == -1) {
+    console.log("setting to soemthing");
+    stopProcess = currentPgm.toString().padStart(3, '0');
+  }
+
+  currentPgm = pgm;
+  fs.writeFileSync('PGM', pgm);
+
+  pm2.stop(stopProcess, function(err) {
+    if(err) {
+      // console.log(err);
+    }
+
+    console.log('stopped');
+  });
+
+  pm2.start({
+    name: startProcess,
+    script: `../${startProcess}/bin/${startProcess}`,
+    watch: false,
+    interpreter: 'none',
+    args: '',
+    autorestart: true
+  }, function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
+}
