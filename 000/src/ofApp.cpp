867e2445e5394353a49ec4f24d8ea1ee8fc05762
davydka <davydka@gmail.com>
2018-09-11 22:15:54 -0400

    rando colors

--- 000/src/ofApp.cpp
+++ 000/src/ofApp.cpp
@@ -105,7 +105,7 @@ void ofApp::draw(){
 
   ofSetColor(255,0,15);
   // ofDrawRectangle(temp,0,1,1);
-  for(int x=0; x<16; x++) {
+  for(int x=0; x<10; x++) {
     for(int y=0; y<16; y++) {
       ofSetColor(
         ofRandom(0,16),
