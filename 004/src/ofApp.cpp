1a07227980c0d3c8973d02ea3ec54fbd2fc13c8b
davydka <davydka@gmail.com>
2018-09-22 16:45:54 -0400

    LIC

--- 004/src/ofApp.cpp
+++ 004/src/ofApp.cpp
@@ -152,7 +152,7 @@ void ofApp::draw(){
     if(indexInt == 3) {
       ofSetColor(0, 0, 0);
       ofDrawRectangle(8, 3, 1, 1);
-      ofDrawRectangle(8, 3, 4, 4);
+      ofDrawRectangle(8, 2, 1, 1);
     }
     if(indexInt == 4) {
       ofSetColor(0, 0, 0);
