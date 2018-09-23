db29ed9aa98517118c193e92f834374db42e2b0e
davydka <davydka@gmail.com>
2018-09-23 15:49:45 -0400

    part time friends

--- 003/src/ofApp.cpp
+++ 003/src/ofApp.cpp
@@ -1,5 +1,15 @@
 #include "ofApp.h"
 
+string INDEX;
+string MIDIPORT;
+string ROT;
+string FLIP;
+int indexInt = 0;
+int rot = 0;
+int cNote = 1; // current note
+int flip = 0;
+float temp = 0;
+
 //--------------------------------------------------------------
 void ofApp::setup() {
   ofBackground(0, 0, 0);                      // default background to black / LEDs off
@@ -9,6 +19,30 @@ void ofApp::setup() {
   ofSetWindowPosition(0, 0);
 
   // SYSTEM SETTINGS
+  INDEX = ofGetEnv("INDEX");
+  MIDIPORT = ofGetEnv("MIDIPORT");
+  ROT = ofGetEnv("ROT");
+  FLIP = ofGetEnv("FLIP");
+  indexInt = ofToInt(INDEX);
+  rot = ofToInt(ROT);
+  flip = ofToInt(FLIP);
+  cout << "pifull INDEX: " << INDEX << endl;
+  cout << "MIDIPORT: " << MIDIPORT << endl;
+  cout << "ROT: " << ROT << endl;
+  cout << "FLIP: " << FLIP << endl;
+
+  midiIn.listInPorts();
+  if( MIDIPORT != "none") {
+    for( int i = 0; i < midiIn.getNumInPorts(); i++ ) {
+      if( ofIsStringInString(midiIn.getInPortName(i), MIDIPORT) ) {
+        midiIn.openPort(i);
+        midiIn.addListener(this);
+      }
+      // cout << midiIn.getInPortName(i) << endl;
+    }
+  }
+
+  // Teensy SYSTEM SETTINGS
   //--------------------------------------
   stripWidth = 16;                            // pixel width of strip
   stripHeight = 128;                            // pixel height of strip
@@ -23,12 +57,13 @@ void ofApp::setup() {
   /* Configure our teensy boards (portName, xOffset, yOffset, width%, height%, direction) */
   //teensy.serialConfigure("cu.usbmodem2809741", 0, 0, 100, 100, 0);
   //teensy.serialConfigure("cu.usbmodem2733511", 0, 0, 100, 100, 0);
-  teensy.serialConfigure("ttyACM0", 0, 0, 100, 100, 0);
+  teensy.serialConfigure("ttyACM0", 0, 0, 100, 100, flip);
   teensy.setBrightness(brightness);
 
 
   // allocate our pixels, fbo, and texture
   fbo.allocate(stripWidth, stripHeight*stripsPerPort*numPorts, GL_RGB);
+  fdbk.allocate(stripWidth, stripHeight*stripsPerPort*numPorts, GL_RGB);
 
   stars.resize(80);
   for(int i=0; i < stars.size(); i++) {
@@ -41,15 +76,17 @@ void ofApp::setup() {
 
 //--------------------------------------------------------------
 void ofApp::update(){
-  for(int i=0; i < stars.size(); i++) {
-    stars[i].z = stars[i].z - .5;
-    stars[i].w = stars[i].z;
-
-    if(stars[i].z < 1) {
-      stars[i].x = ofRandom(-stripWidth, stripWidth);
-      stars[i].y = ofRandom(-rowHeight, rowHeight);
-      stars[i].z = ofRandom(0, stripWidth);
+  if( cNote == 100) {
+    for(int i=0; i < stars.size(); i++) {
+      stars[i].z = stars[i].z - .3;
       stars[i].w = stars[i].z;
+
+      if(stars[i].z < 1) {
+        stars[i].x = ofRandom(-stripWidth, stripWidth);
+        stars[i].y = ofRandom(-rowHeight, rowHeight);
+        stars[i].z = ofRandom(0, stripWidth);
+        stars[i].w = stars[i].z;
+      }
     }
   }
 
@@ -63,10 +100,57 @@ void ofApp::draw(){
   ofSetColor(255);
 
   ofPushMatrix();
-  ofTranslate( stripWidth / 2.f, rowHeight / 2.f );
-  for(int i=0; i < stars.size(); i++) {
-    star(stars[i].x, stars[i].y, stars[i].z, stars[i].w);
+  ofTranslate(8, 8);
+  ofRotateZDeg(rot);
+  ofTranslate(-8, -8);
+
+  if( cNote == 1 ) {
+    ofPushMatrix();
+      ofTranslate(8,8,0);
+      ofSetColor(255, 100, 32);
+      ofFill();
+      ofSetPolyMode(OF_POLY_WINDING_ODD);
+      ofBeginShape();
+      float angleStep = TWO_PI/(10.0f + sin(ofGetElapsedTimef()/5.0f) * 60);
+      float radiusAdder = 0.5f;
+      float radius = 0;
+      for (int i = 0; i < 20; i++){
+        float anglef = (i) * angleStep;
+        float x = radius * cos(anglef);
+        float y = radius * sin(anglef);
+        ofVertex(x,y);
+        radius += radiusAdder;
+      }
+      ofEndShape(OF_CLOSE);
+    ofPopMatrix();
   }
+
+  if( cNote != 100 ) {
+     if(indexInt == 3) {
+      ofSetColor(0, 0, 0);
+
+      ofDrawRectangle(8, 3, 1, 1);
+
+      // ofDrawRectangle(8, 2, 1, 1);
+      ofDrawRectangle(9, 3, 1, 1);
+    }
+    if(indexInt == 4) {
+      ofSetColor(0, 0, 0);
+      ofDrawRectangle(14, 7, 1, 1);
+    }
+  }
+
+  if( cNote == 100 ) {
+    ofPushMatrix();
+    ofTranslate( stripWidth / 2.f, rowHeight / 2.f );
+    for(int i=0; i < stars.size(); i++) {
+      star(stars[i].x, stars[i].y, stars[i].z, stars[i].w);
+    }
+    ofPopMatrix();
+  }
+
+
+
   ofPopMatrix();
   fbo.end();
 
@@ -83,6 +167,39 @@ void ofApp::star(float x, float y, float z, float w) {
   float sy = ofMap(y / z, 0, 1, 0, rowHeight);
   float r = ofMap(z, 0, ofGetWidth(), 2, 0);
   ofDrawCircle(sx, sy, r);
+}
+
+//--------------------------------------------------------------
+void ofApp::newMidiMessage(ofxMidiMessage& msg) {
+  if( msg.channel != 6 ) {
+    return;
+  }
+  if( msg.status == MIDI_NOTE_ON && msg.velocity > 0 ) {
+    handleNote(msg.pitch);
+  }
+  if( msg.status == MIDI_CONTROL_CHANGE ) {
+    // cout << msg.control << endl;
+    // cout << msg.value << endl;
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::handleNote(int note) {
+  if( note == cNote ) {
+    // return;
+  }
+  cNote = note;
+  cout << note << endl;
+  cout << temp << endl;
+  temp = 0;
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+  if( MIDIPORT != "none") {
+    midiIn.closePort();
+    midiIn.removeListener(this);
+  }
 }
 
 //--------------------------------------------------------------
--- 003/src/ofApp.h
+++ 003/src/ofApp.h
@@ -2,30 +2,54 @@
 
 #include "ofxTeensyOcto.h"
 #include "ofMain.h"
+#include "ofxMidi.h"
 
-class ofApp : public ofBaseApp{
+typedef struct {
 
-	public:
-		void setup();
-		void update();
-		void draw();
-		void gotMessage(ofMessage msg);
+  float 	x;
+  float 	y;
+  bool 	bBeingDragged;
+  bool 	bOver;
+  float 	radius;
 
-		// LED - Teensy stuff
-		ofxTeensyOcto teensy;
-		int stripWidth;
-		int stripHeight;
-		int rowHeight;
-		int stripsPerPort;
-		int numPorts;
-		int brightness;
+}draggableVertex;
 
-		// FBO stuff
-		//-----------------------------
-		ofFbo fbo;
+class ofApp : public ofBaseApp, public ofxMidiListener{
 
-		// Graphic functions
-		//-----------------------------
-		void star(float x, float y, float z, float w);
-		vector <ofVec4f> stars;
+  public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    void gotMessage(ofMessage msg);
+
+    // MIDI stuff
+    ofxMidiIn midiIn;
+    void newMidiMessage(ofxMidiMessage& eventArgs);
+    void handleNote(int note);
+    std::vector<ofxMidiMessage> midiMessages;
+    std::size_t maxMessages = 10; // messages to keep track of
+
+    // LED - Teensy stuff
+    ofxTeensyOcto teensy;
+    int stripWidth;
+    int stripHeight;
+    int rowHeight;
+    int stripsPerPort;
+    int numPorts;
+    int brightness;
+
+    // FBO stuff
+    //-----------------------------
+    ofFbo fbo;
+    ofFbo fdbk;
+
+    // Graphic functions
+    //-----------------------------
+    void star(float x, float y, float z, float w);
+    vector <ofVec4f> stars;
+
+    int nCurveVertices;
+    draggableVertex curveVertices[7];
+    draggableVertex bezierVertices[4];
 };
